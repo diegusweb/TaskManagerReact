87e89c2ce38ecae7e48219b2c43ec5713ae788d9
Diego Rueda <diego.rueda2@gmail.com>
2019-02-25 14:47:02 -0400

    Marking a task as completed

--- public/js/app.js
+++ public/js/app.js
@@ -66113,9 +66113,9 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
@@ -66137,8 +66137,15 @@ function (_Component) {
     _this = _possibleConstructorReturn(this, _getPrototypeOf(SingleProject).call(this, props));
     _this.state = {
       project: {},
-      tasks: []
+      tasks: [],
+      title: '',
+      errors: []
     };
+    _this.handleFieldChange = _this.handleFieldChange.bind(_assertThisInitialized(_this));
+    _this.handleAddNewTask = _this.handleAddNewTask.bind(_assertThisInitialized(_this));
+    _this.hasErrorFor = _this.hasErrorFor.bind(_assertThisInitialized(_this));
+    _this.renderErrorFor = _this.renderErrorFor.bind(_assertThisInitialized(_this));
+    _this.handleMarkProjectAsCompleted = _this.handleMarkProjectAsCompleted.bind(_assertThisInitialized(_this));
     return _this;
   }
 
@@ -66156,8 +66163,82 @@ function (_Component) {
       });
     }
   }, {
+    key: "handleFieldChange",
+    value: function handleFieldChange(event) {
+      this.setState({
+        title: event.target.value
+      });
+    }
+  }, {
+    key: "handleAddNewTask",
+    value: function handleAddNewTask(event) {
+      var _this3 = this;
+
+      event.preventDefault();
+      var task = {
+        title: this.state.title,
+        project_id: this.state.project.id
+      };
+      axios__WEBPACK_IMPORTED_MODULE_0___default.a.post('/api/tasks', task).then(function (response) {
+        // clear form input
+        _this3.setState({
+          title: ''
+        }); // add new task to list of tasks
+
+
+        _this3.setState(function (prevState) {
+          return {
+            tasks: prevState.tasks.concat(response.data)
+          };
+        });
+      }).catch(function (error) {
+        _this3.setState({
+          errors: error.response.data.errors
+        });
+      });
+    }
+  }, {
+    key: "hasErrorFor",
+    value: function hasErrorFor(field) {
+      return !!this.state.errors[field];
+    }
+  }, {
+    key: "renderErrorFor",
+    value: function renderErrorFor(field) {
+      if (this.hasErrorFor(field)) {
+        return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("span", {
+          className: "invalid-feedback"
+        }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("strong", null, this.state.errors[field][0]));
+      }
+    }
+  }, {
+    key: "handleMarkProjectAsCompleted",
+    value: function handleMarkProjectAsCompleted() {
+      var history = this.props.history;
+      axios__WEBPACK_IMPORTED_MODULE_0___default.a.put("/api/projects/".concat(this.state.project.id)).then(function (response) {
+        return history.push('/');
+      });
+    }
+  }, {
+    key: "handleMarkTaskAsCompleted",
+    value: function handleMarkTaskAsCompleted(taskId) {
+      var _this4 = this;
+
+      axios__WEBPACK_IMPORTED_MODULE_0___default.a.put("/api/tasks/".concat(taskId)).then(function (response) {
+        _this4.setState(function (prevState) {
+          return {
+            tasks: prevState.tasks.filter(function (task) {
+              return task.id !== taskId;
+            })
+          };
+        });
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
+      var _this5 = this;
+
       var _this$state = this.state,
           project = _this$state.project,
           tasks = _this$state.tasks;
@@ -66174,15 +66255,32 @@ function (_Component) {
       }, project.name), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
         className: "card-body"
       }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", null, project.description), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("button", {
-        className: "btn btn-primary btn-sm"
-      }, "Mark as completed"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("hr", null), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("ul", {
+        className: "btn btn-primary btn-sm",
+        onClick: this.handleMarkProjectAsCompleted
+      }, "Mark as completed"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("hr", null), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("form", {
+        onSubmit: this.handleAddNewTask
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "input-group"
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("input", {
+        type: "text",
+        name: "title",
+        className: "form-control ".concat(this.hasErrorFor('title') ? 'is-invalid' : ''),
+        placeholder: "Task title",
+        value: this.state.title,
+        onChange: this.handleFieldChange
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "input-group-append"
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("button", {
+        className: "btn btn-primary"
+      }, "Add")), this.renderErrorFor('title'))), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("ul", {
         className: "list-group mt-3"
       }, tasks.map(function (task) {
         return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("li", {
           className: "list-group-item d-flex justify-content-between align-items-center",
           key: task.id
         }, task.title, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("button", {
-          className: "btn btn-primary btn-sm"
+          className: "btn btn-primary btn-sm",
+          onClick: _this5.handleMarkTaskAsCompleted.bind(_this5, task.id)
         }, "Mark as completed"));
       })))))));
     }
